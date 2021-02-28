79750bae511d8a112fd8010b9f6d77c71d2869bb
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-02-28 12:05:54 +0200

    vaimo

--- src/config/Fire.js
+++ src/config/Fire.js
@@ -3,12 +3,12 @@ import firebase from 'firebase';
 const config = {
     apiKey: "AIzaSyBVMVR1xE0LG6hC2rlM5Pu3I_VYAuFL4YE",
     authDomain: "vaimo-f133e.firebaseapp.com",
-    databaseURL: "https://vaimo-f133e.firebaseio.com",
+    //databaseURL: "https://vaimo-f133e.firebaseio.com",
     projectId: "vaimo-f133e",
     storageBucket: "vaimo-f133e.appspot.com",
     messagingSenderId: "93267068249",
-    appId: "1:93267068249:web:b722207ede1c6fa0ac7848",
-    measurementId: "G-8JGLK5WBVM"
+    appId: "1:93267068249:web:cb86c555bba039c8ac7848",
+    measurementId: "G-73F60SE44C"
 };
 
 
